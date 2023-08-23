eb049dd1faef88906a12967c2165acba215cae8c
r2u0t0h4 <akoruth22@gmail.com>
2023-08-23 14:50:49 +0100

    commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/*console.log(document.getElementById('form'));
+
+const form = document.getElementById('form');
+//change styling
+
+form.style.background ='yellow';
+form.innerHTML += '<form style="display:block;"><h1>Form Element</form>';
+console.log(document.querySelector('#form'));
+document.querySelector('li:last-child').style.color = 'red';*/
+
+const item =document.getElementsByClassName('collection-item')
+console.log(item);
---
+++ javascript/app.js
@@ -0,0 +1,6 @@
+let val;
+ val = document;
+
+
+
+console.log(val);
