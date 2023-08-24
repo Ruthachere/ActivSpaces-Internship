321c0da9026ce5f1819ec26ddd63e141cf6e24d5
r2u0t0h4 <akoruth22@gmail.com>
2023-08-24 09:22:32 +0100

    commit

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+//event listeners for from
 const form = document.querySelector('#form_control');
-form.addEventListener('submit' ,runEvent);
-
-const taskInput =document.querySelector('.task');
-taskInput.value ='';
-taskInput.addEventListener('keydown',runEvent);
+form.addEventListener('submit', runEvent);
+//event listeners for taskInput
+const taskInput = document.querySelector('.task');
+//clear input
+taskInput.value = '';
+taskInput.addEventListener('input', runEvent);
 
 
  function runEvent (e){
-console.log(`EVENT TYPE: &{e.type}`);
- console.log(taskInput.value);
+console.log(`EVENT TYPE: ${e.type}`);
+//get input value
+console.log(taskInput.value);
+ 
  e.preventDefault();
 }
