6a0dbb08859169c63eb03df2610e1990f304399d
r2u0t0h4 <akoruth22@gmail.com>
2023-08-24 12:32:35 +0100

    commit

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
-//event listeners for from
+//define ui variables
 const form = document.querySelector('#form_control');
-form.addEventListener('submit', runEvent);
-//event listeners for taskInput
-const taskInput = document.querySelector('.task');
-//clear input
+var taskInputs = document.querySelector('.task');
+const textarea =document.querySelector('#description');
+
+var taskInputs = document.querySelector('#tasks');
+
+//load all event listeners
+loadEventListeners();
+
+//load all event listeners
+function  loadEventListeners() {
+    //add form event
+    form.addEventListener('submit', addTask); 
+     
+}
+//add task
+function addTask(e) {
+    if (taskInputs.value === '' ) {
+        alert('Add a task'); 
+       
+    }
+   
+    console.log(`EVENT TYPE: ${e.type}`);
+    //clear input
+    taskInputs.value = '';
+    e.preventDefault();   
+}
+
+
+
+ 
+
+/* clear input
 taskInput.value = '';
 taskInput.addEventListener('input', runEvent);
 
+//event listeners for textarea
+
+textarea.addEventListener('', runEvent);
+textarea.value = '';
 
  function runEvent (e){
-console.log(`EVENT TYPE: ${e.type}`);
-//get input value
+
 console.log(taskInput.value);
+ console.log(textarea.value);
  
- e.preventDefault();
-}
+}*/
+
+
