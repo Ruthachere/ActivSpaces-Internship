a4008fec665e901875f0c6a5cb64434ab369d17f
r2u0t0h4 <akoruth22@gmail.com>
2023-08-25 17:12:58 +0100

    commit

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 //define ui variables
 const form = document.querySelector('form');
-// const taskInput = document.querySelector('.task');
-// const times = document.querySelector('#time');
-// const dates = document.querySelector('#date');
-// const desc = document.querySelector('#description')
 var wrapper = document.getElementById('todo_wrapper');
 
-
+//creating th inner html 
 const generateTemplate = (todo) =>{
 
     var html =  `<div class="col bg-white border">
     <div class="col-10 d-flex " style="justify-content: end;">
     <p class="me-5">Date:${todo.date}</p>
-    <p>Time:</label></p>
+    <p>Time:${todo.time}</label></p>
     </div>
     <div class="col-12">
-    <h6>Task${todo.taskInput}</h6>
-    <h6>Task Description</h6>
+    <h6>Task:${todo.taskInput}</h6>
+    <h6>Task Description:${todo.description}</h6>
     <hr>
     <div class="col-12 d-flex"  style="justify-content: end;">
     <button class="btn btn-2 border border-dark" id="btn-2">Delete</button>
@@ -27,20 +23,24 @@ const generateTemplate = (todo) =>{
     wrapper.innerHTML += html;
     
 };
-
+//creating the objects fot the innerHtml
 form.addEventListener('submit', e => {
     e.preventDefault();
     let taskInput = form.names.value.trim();
     let date = form.date.value.trim();
-    console.log({taskInput: taskInput, date: date});
+    let time = form.time.value.trim();
+    let description = form.description.value.trim();
+    console.log({taskInput: taskInput, date: date, time: time, description: description});
 
-
-
-
-    generateTemplate({taskInput: taskInput, date: date});
+    generateTemplate({
+        taskInput: taskInput, 
+        date: date, 
+        time: time, 
+        description: description
+    });
     form.reset();
 });
-// const clearBtn = document.querySelector('form');
+// const clearBtn = document.querySelector('#names');
 // clearBtn.addEventListener('click',clearTasks);
 // function clearTasks() {
 //     while(clearBtn.firstChild){
