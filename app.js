c6f44628a5dffcf012294f974f7fc6a648eacac5
r2u0t0h4 <akoruth22@gmail.com>
2023-08-25 16:29:39 +0100

    commit

--- app.js
+++ app.js
@@ -1,101 +1,87 @@
 //define ui variables
-const form = document.querySelector('#form_control');
+const form = document.querySelector('form');
+// const taskInput = document.querySelector('.task');
+// const times = document.querySelector('#time');
+// const dates = document.querySelector('#date');
+// const desc = document.querySelector('#description')
+var wrapper = document.getElementById('todo_wrapper');
 
 
+const generateTemplate = (todo) =>{
 
-
-
-/*const taskList = document.querySelector('.collection') ;
-const taskInput = document.querySelector('#tasks');
-const clearBtn = document.querySelector('#btn-2')
-//const taskTitle = document.querySelector('#Task');
-
-
-//definition of ui values for date
-const taskListOne = document.querySelector('.collection') ;
-const taskInputOne = document.querySelector('#task');
-//const taskTitleOne = document.querySelector('#Task-one');
-
-//definition of ui values for time
-const taskListTwo = document.querySelector('.collection-two') ;
-const taskInputTwo = document.querySelector('#task-1');
-//const taskTitleTwo = document.querySelector('#Task-two');
-
-//definition of ui values for textarea
-const taskListThree = document.querySelector('.collection-three') ;
-const taskInputThree = document.querySelector('#description');
-//const taskTitleThree = document.querySelector('#task-three');
-//load all event listeners
-loadEventListeners();
-
-//load all event listeners
-function  loadEventListeners() {
-    //add task event
-    form.addEventListener('submit', addTask); 
-    clearBtn.addEventListener('click',clearTasks);
-
-}
-//add task
-function addTask(e) {
-    if (taskInput.value === '' ) {
-        alert('Add a task'); 
-       
-    }
-    //create text element for date
-    var text = document.createElement('text');
-
-    //add class for text
-    text.className = 'collection-items';
-    //cretae text node and append to text for date
-    text.appendChild(document.createTextNode(taskInputOne.value));
-     //append  text to ul
-    taskListOne.appendChild(text);
-    taskInputOne.value = '';
-   text.style.display = 'block';
+    var html =  `<div class="col bg-white border">
+    <div class="col-10 d-flex " style="justify-content: end;">
+    <p class="me-5">Date:${todo.date}</p>
+    <p>Time:</label></p>
+    </div>
+    <div class="col-12">
+    <h6>Task${todo.taskInput}</h6>
+    <h6>Task Description</h6>
+    <hr>
+    <div class="col-12 d-flex"  style="justify-content: end;">
+    <button class="btn btn-2 border border-dark" id="btn-2">Delete</button>
+    <button class="btn btn-2 border border-dark">Edit</button>
+    </div>
+    </div>
+    </div>`;
+    wrapper.innerHTML += html;
     
+};
 
-    //create text element for time
-    var text = document.createElement('text');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let taskInput = form.names.value.trim();
+    let date = form.date.value.trim();
+    console.log({taskInput: taskInput, date: date});
 
-    //add class
-    text.className = 'collection-item-one';
-    //create text node and append to li
-    text.appendChild(document.createTextNode(taskInputTwo.value));
-     //append to ul
-    taskListTwo.appendChild(text);
-    taskInputTwo.value = '';
-    text.style.display = 'block';
 
-    //create text element for task
-    var text = document.createElement('text');
-    //add class
-    text.className = 'collection-item';
-    //create text node and append to text
-    text.appendChild(document.createTextNode(taskInput.value));
-   //append  text to ul
-   taskList.appendChild(text);
-   taskInput.value = '';
-   text.style.display = 'block';
 
-    //create text element for task
-    var text = document.createElement('text');
-    //add class
-    text.className = 'collection-item-three';
-    //create text node and append to text
-    text.appendChild(document.createTextNode(taskInputThree.value));
-   //append  text to ul
-   taskListThree.appendChild(text);
-   taskInputThree.value = '';
-   text.style.display = 'block';
-   
-   e.preventDefault();   
-}
-   //clear input
-   function clearTasks() {
-     while(taskList.firstChild){
-        taskList.removeChild(taskList.firstChild);
-     }
-   }
+
+    generateTemplate({taskInput: taskInput, date: date});
+    form.reset();
+});
+// const clearBtn = document.querySelector('form');
+// clearBtn.addEventListener('click',clearTasks);
+// function clearTasks() {
+//     while(clearBtn.firstChild){
+//        clearBtn.removeChild(clearBtn.firstChild);
+//     }
+//   }
+
+
+
+
+
+// var toDo = document.getElementById('todo');
+// toDo.innerHTML += `<div class="col bg-white border">
+// <div class="col-10 d-flex " style="justify-content: end;">
+// <p class="me-5">Date:${dates}</p>
+// <p>Time:${times}</label></p>
+// </div>
+// <div class="col-12">
+// <h6>Task${taskInput}</h6>
+// <h6>Task Description${desc}</h6>
+// <hr>
+// <div class="col-12 d-flex"  style="justify-content: end;">
+// <button class="btn btn-2 border border-dark" id="btn-2">Delete</button>
+// <button class="btn btn-2 border border-dark">Edit</button>
+// </div>
+// </div>
+// </div>`;
+
+// form.addEventListener('click', addTask); 
+// function addTask(e) {
+//     todo = form.value;
+//     console.log(todo);
+//     // if (taskInput.value === '' ) {
+//     //     alert('Add a task'); 
+//        // }
+//      taskInput.value = '';
+//     e.preventDefault();   
+// }
+
+
+
 
 
 
