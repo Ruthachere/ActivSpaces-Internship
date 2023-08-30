ce71a0401d0006ef7871a65d8eaad95e0ba9773f
r2u0t0h4 <akoruth22@gmail.com>
2023-08-30 11:15:59 +0100

    commit

--- app.js
+++ app.js
@@ -1,69 +1,101 @@
-//define ui variables
-const form = document.querySelector('form');
-var wrapper = document.getElementById('todo_wrapper');
 
-//creating th inner html 
-const generateTemplate = (todo) =>{
+let tasks = []; // Array of tasks => [{title:"Create todo",desc: "create todo using javascript",date: "25-08-2023",time: "5:38"}]
+let form = document.getElementById("task-form"); //declaring
 
-    var html =  `<div class="col bg-white border">
-    <div class="col-lg-10 col-md-12 d-flex " style="justify-content: end;">
-    <p class="me-5">Date:${todo.date}</p>
-    <p>Time:${todo.time}</label></p>
-    </div>
-    <div class="col-12">
-    <h6>Task:${todo.taskInput}</h6>
-    <p>Task Description:${todo.description}</p>
-    <hr>
-    <div class="col-lg-12 col-md-9 d-flex"  style="justify-content: end;">
-    <button class="btn border edit-btn" data-index="index">Edit</button>
-    <button class="btn border delete-btn" data-index="index">Delete</button>
-    </div>
-    </div>
+console.log("Hello Pixel magicians");
+
+function displayTasks(listOfTasks) { // listOfTasks = tasks
+    //todo
+    //create html variable to store the html contents
+    //loop through task to create html content
+    //override html content of task wrapper with generated html;
+
+    document.getElementById("tasks").innerHTML = "";
+    // html += `            
+    // // <div class="list-of-tasks">`;
+    for (let i = 0; i < listOfTasks.length; i++) {
+        let html = `<div class="task_1">
+        <div class="task-header">
+            <h5 class="task-title m-0">${listOfTasks[i].taskName}</h5>
+            <div class="date-wrapper">
+                <span>${listOfTasks[i].date}:</span>
+                <span>${listOfTasks[i].time}</span>
+            </div>
+        </div>
+        <p class="task-desc">${listOfTasks[i].taskDesc}</p>
+        <hr>
+        <div class="task-footer actions ">
+            <button  onclick="editTodo(${i})"><i></i>Edit</button>
+            <button onclick="deleteTodo(${i})">Delete</button>
+        </div>
     </div>`;
-    wrapper.innerHTML += html;
-    
-};
-//creating the objects for the innerHtml
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    let taskInput = form.names.value.trim();
-    let date = form.date.value.trim();
-    let time = form.time.value.trim();
-    let description = form.description.value.trim();
-    console.log({taskInput: taskInput, date: date, time: time, description: description});
-
-    generateTemplate({
-        taskInput: taskInput, 
-        date: date, 
-        time: time, 
-        description: description
-    });
-    form.reset();
-});
-
- // Event listener to edit amd delete the todo
- wrapper.addEventListener('click', (e) => {
-    if (e.target.classList.contains('edit-btn')) {
-        const index = e.target.getAttribute('data-index');
-        const updatedTodo = prompt('Edit todo:', wrapper[index]);
-        if (updatedTodo !== null) {
-            wrapper[index] = updatedTodo;
-        //    saveTodosToLocalStorage();
-        }
-    } else if (e.target.classList.contains('delete-btn')) {
-        const index = e.target.getAttribute('data-index');
-        wrapper.firstElementChild.remove(index, 0);
-    //    saveTodosToLocalStorage();
+    document.getElementById("tasks").innerHTML += html;
     }
-});
-   // Retrieve todos from local storage or initialize an empty array
-   let todos = JSON.parse(localStorage.getItem('todo_wrapper')) || [];
+    // html += `</div>`;
 
- // Function to save todos to local storage
- function saveTodosToLocalStorage() {
-    localStorage.setItem('todo_wrapper', JSON.stringify(todos));
-    updateTodoList();
+    
 }
+
+
+form.addEventListener("submit", function (e) {
+    //todo
+    // 1-prevent default form submission
+    // get form values
+    // validate form
+    //create an object of task with a unique id
+    // add object to list of tasks
+    // call a function to dsiplay task
+    e.preventDefault();
+    const taskName = form.task_name.value;
+    const taskDesc = form.desccription.value;
+    const date = form.date.value;
+    const time = form.time.value;
+    const taskId =form.task_id.value; 
+    const task = {
+       // id: taskName.toLowerCase().replace(" ", "-"),
+        taskName: taskName,
+        taskDesc: taskDesc,
+        date: date,
+        time: time
+    }
+    if (taskId) {
+        tasks[taskId] = task;
+        
+    } else {
+        tasks.push(task);
+    }
+
+   
+    form.reset();
+   displayTasks(tasks); // function call
+    let array = JSON.stringify(tasks);
+    localStorage.setItem('task', array);
+});
+
+if(localStorage.getItem('task')){
+    array = localStorage.getItem('task');
+    tasks = JSON.parse(array);   
+}
+displayTasks(tasks);
+function deleteTodo(i) {
+    tasks.splice(i, 1);
+    localStorage.setItem('task',JSON.stringify(tasks));
+    displayTasks(tasks);
+}
+// function editTodo(i){
+//     const todo = tasks[i];
+//     document.querySelector('#task_name').value = listOfTasks[i].taskName;
+// }
+function editTodo(i){
+    const todo = tasks[i];
+    form.task_name.value = todo.taskName;
+    form.desccription.value = todo.taskDesc;
+     form.date.value = todo.date;
+   form.time.value = todo.time;
+   form.task_id.value = i;
+}
+
+
 
 
 
