4ee78c76071dc4290b69eab2d042a89adc1c53c1
r2u0t0h4 <akoruth22@gmail.com>
2023-08-31 09:49:05 +0100

    commit

--- app.js
+++ app.js
@@ -7,12 +7,8 @@ console.log("Hello Pixel magicians");
 function displayTasks(listOfTasks) { // listOfTasks = tasks
     //todo
     //create html variable to store the html contents
-    //loop through task to create html content
-    //override html content of task wrapper with generated html;
-
     document.getElementById("tasks").innerHTML = "";
-    // html += `            
-    // // <div class="list-of-tasks">`;
+    //loop through task to create html content
     for (let i = 0; i < listOfTasks.length; i++) {
         let html = `<div class="task_1">
         <div class="task-header">
@@ -29,63 +25,107 @@ function displayTasks(listOfTasks) { // listOfTasks = tasks
             <button onclick="deleteTodo(${i})">Delete</button>
         </div>
     </div>`;
+    //override html content of task wrapper with generated html;
     document.getElementById("tasks").innerHTML += html;
     }
-    // html += `</div>`;
-
     
 }
 
-
+//eventlistener for the submit button
 form.addEventListener("submit", function (e) {
-    //todo
     // 1-prevent default form submission
-    // get form values
-    // validate form
-    //create an object of task with a unique id
-    // add object to list of tasks
-    // call a function to dsiplay task
     e.preventDefault();
+    // get form values
     const taskName = form.task_name.value;
     const taskDesc = form.desccription.value;
     const date = form.date.value;
     const time = form.time.value;
-    const taskId =form.task_id.value; 
+    const taskId = form.task_id.value;
+    
+    // add object to list of tasks
+    
+    
+    let hasError = false;
+    // validation for form
+    //validation for the task name
+    if (taskName == "") {
+        document.getElementById('div1').innerHTML ="Please fill out the task name ";
+        hasError = true;
+    } 
+    //else if
+    else{
+        document.getElementById('div1').innerHTML = "";
+    }
+    //validation of the task Description
+    if (taskDesc == "") {
+        document.getElementById('div2').innerHTML ="Please fill out the task description ";
+        hasError = true;
+    } 
+    //else if
+    else{
+        document.getElementById('div2').innerHTML ="";
+    }
+//validation of the date
+    if (date == "") {
+        document.getElementById('div3').innerHTML ="Please fill";
+        hasError = true;
+    } 
+    //else if
+    else{
+        document.getElementById('div3').innerHTML ="";
+    }
+    //validation of time
+    if (time == "") {
+        document.getElementById('div4').innerHTML ="Please fill";
+        hasError = true;
+    } 
+    //else if
+    else{
+        document.getElementById('div4').innerHTML ="";
+    }
+    if (hasError) {
+        return;
+    }
+    //create an object of task with a unique 
     const task = {
-       // id: taskName.toLowerCase().replace(" ", "-"),
         taskName: taskName,
         taskDesc: taskDesc,
         date: date,
         time: time
     }
+    //condition for the edit
     if (taskId) {
         tasks[taskId] = task;
         
     } else {
         tasks.push(task);
     }
-
-   
+    
     form.reset();
-   displayTasks(tasks); // function call
+    // call a function to dsiplay task
+   displayTasks(tasks); 
     let array = JSON.stringify(tasks);
     localStorage.setItem('task', array);
 });
 
+
+//storing of values in the local storage
 if(localStorage.getItem('task')){
     array = localStorage.getItem('task');
     tasks = JSON.parse(array);   
 }
 displayTasks(tasks);
+
+
+//function to delete todo
 function deleteTodo(i) {
     tasks.splice(i, 1);
     localStorage.setItem('task',JSON.stringify(tasks));
     displayTasks(tasks);
 }
-// function editTodo(i){
-//     const todo = tasks[i];
-//     document.querySelector('#task_name').value = listOfTasks[i].taskName;
-// }
+
+
+// function editTodo
 function editTodo(i){
     const todo = tasks[i];
     form.task_name.value = todo.taskName;
@@ -94,23 +134,3 @@ function editTodo(i){
    form.time.value = todo.time;
    form.task_id.value = i;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
